6ca4035a36f635c9a8a11c3ffbee111952df4ea9
SyntheticMatrix <zxy930709@gmail.com>
2025-04-08 22:06:12 +0800

    Add files via upload

---
+++ stationary_distribution_spp1.m
@@ -0,0 +1,47 @@
+figure('Color','w','Position',[100 100 1200 450]);
+
+% =====================
+% 
+% =====================
+illiquidWealth = W1_ill_matrix_old(:,12);
+liquidWealth = W1_liq_matrix_old(:,12);
+new_asset.illquid = W1_ill_matrix(:,12);
+new_asset.liquid = W1_liq_matrix(:,12);
+binWidth = 0.1;
+binEdges = 0:binWidth:max([illiquidWealth; new_asset.illiquid+new_asset.liquid]);
+
+
+histogram(illiquidWealth,...
+    'BinEdges', binEdges,...
+    'Normalization', 'probability',...
+    'FaceColor', [0.2 0.2 1],...
+    'FaceAlpha', 0.7,...
+    'EdgeColor', 'none');
+hold on;
+
+
+histogram(new_asset.illiquid+new_asset.liquid,...
+    'BinEdges', binEdges,...
+    'Normalization', 'probability',...
+    'FaceColor', [1 0.6 0],...
+    'FaceAlpha', 0.7,...
+    'EdgeColor', 'none');
+simulated_mean = mean(illiquidWealth+liquidWealth); 
+your_original_illiquid_data = w1_liq_real + w1_ill_real;
+
+original_mean = mean(w1_liq_real+w1_ill_real);
+
+
+delta_illiquid = mean(illiquidWealth) - mean(your_original_illiquid_data);
+xIlliquidShifted = xIlliquidOriginal + delta_illiquid;
+
+
+[fIlliquidOriginal, xIlliquidOriginal] = ksdensity(your_original_illiquid_data);
+plot(xIlliquidShifted, fIlliquidOriginal * binWidth, 'k--', 'LineWidth', 1.5);
+
+yticks = get(gca, 'YTick');
+set(gca, 'YTickLabel', yticks * 100);
+ylabel('Percent (%)');
+xlabel('Wealth');
+title('Illiquid Wealth Distribution');
+legend({'Time-lag Debt', 'Standard Debt', 'Theoretical'}, 'Location', 'northeast');
---
+++ stationary_distribution_spp2.m
@@ -0,0 +1,47 @@
+figure('Color','w','Position',[100 100 1200 450]);
+
+% =====================
+% 
+% =====================
+illiquidWealth2 = W2_ill_matrix_old(:,12);
+liquidWealth2 = W2_liq_matrix_old(:,12);
+new_asset.illquidt = W2_ill_matrix(:,12);
+new_asset.liquid2 = W2_liq_matrix(:,12);
+binWidth = 0.1;
+binEdges = 0:binWidth:max([illiquidWealth2+liquidWealth2; new_asset.illiquidt+new_asset.liquid2]);
+
+% 
+histogram(illiquidWealth2+liquidWealth2,...
+    'BinEdges', binEdges,...
+    'Normalization', 'probability',...
+    'FaceColor', [0.2 0.2 1],...
+    'FaceAlpha', 0.7,...
+    'EdgeColor', 'none');
+hold on;
+
+% 
+histogram(new_asset.illiquidt+new_asset.liquid2,...
+    'BinEdges', binEdges,...
+    'Normalization', 'probability',...
+    'FaceColor', [1 0.6 0],...
+    'FaceAlpha', 0.7,...
+    'EdgeColor', 'none');
+simulated_mean = mean(illiquidWealth2+liquidWealth2); 
+your_original_illiquid_data = w2_liq_real + w2_ill_real;
+
+original_mean = mean(w2_liq_real+w2_ill_real);
+
+
+delta_illiquid = mean(illiquidWealth2+liquidWealth2) - mean(your_original_illiquid_data);
+xIlliquidShifted = xIlliquidOriginal + delta_illiquid;
+
+[fIlliquidOriginal, xIlliquidOriginal] = ksdensity(your_original_illiquid_data);
+plot(xIlliquidShifted, fIlliquidOriginal * binWidth, 'k--', 'LineWidth', 1.5);
+
+
+yticks = get(gca, 'YTick');
+set(gca, 'YTickLabel', yticks * 100);
+ylabel('Percent (%)');
+xlabel('Wealth');
+title('Illiquid Wealth Distribution');
+legend({'Time-lag Debt', 'Standard Debt', 'Theoretical'}, 'Location', 'northeast');
