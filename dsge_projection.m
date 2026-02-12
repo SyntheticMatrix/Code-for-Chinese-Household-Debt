c0c3cb832c20169bcb573fc1a5e44373cbeb4955
SyntheticMatrix <zxy930709@gmail.com>
2026-02-12 11:37:50 +0800

    Update dsge_projection.m

--- dsge_projection.m
+++ dsge_projection.m
@@ -71,7 +71,7 @@ B(row, 2) = -omega;
 beta = 0.98;
 kappa = 0.0823;
 row = 7;
-B(row, 7) = 1;   % pi(t)
+B(row, 7) = -1;   % pi(t)
 A(row, 7) = beta; % -beta*E_t[pi(t+1)]
 B(row, 6) = kappa+0.008; % -kappa*m(t)
 
@@ -129,4 +129,5 @@ IRPJ_H=ir(f,p,x2_2,100);
 f_indirect = f;
 f_indirect(:,5) = 0;
 IRPJ_HIE=ir(f_indirect,p,x2_2,100);
+
 
